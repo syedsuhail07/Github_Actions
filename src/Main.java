6df5fffbaca0b6c71dc3704647d5206420dc6c01
syed <syedsuhail8846@gmail.com>
2024-10-31 16:41:21 +0530

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main(String[] args) {
+        // Hardcoded input
+        float a1w = 60f;      
+        float a2w = 70f;
+        
+         condition ? true : false;
+        
+        // Output the result
+        System.out.println("a2w: " + a2w);
+        System.out.println("a1w: " + a1w);
+    }
+
+}
